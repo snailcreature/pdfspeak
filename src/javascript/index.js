200a3d60c8cfb1c312c443065c91542a71376584
snailcreature <samuel.drage@myport.ac.uk>
2023-02-15 18:53:12 +0000

    Initial commit

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,29 @@
 import '../css/index.css';
 
-console.log("Hello, World!")
+import fs from 'fs';
+import PDFJS from 'pdfjs-dist';
+import { parse } from 'path';
+
+console.log("Hello, World!");
+
+// const voices = speechSynthesis.getVoices();
+
+// const voiceList = document.querySelector('#voice-list');
+// voices.forEach((voice) => {
+//   voiceList.innerHTML += `<li>${voice.name}</li>`
+// });
+
+const pdfTextP = document.querySelector('#pdf-text');
+
+PDFJS.getDocument('./PCODE_UP882151.pdf').then((pdf) => {
+  for (let i = 1; i <= pdf.numPages; i++) {
+    pdf.getPage(i).then((page) => {
+      page.getTextContent().then((textContent) => {
+        pdfTextP.innerHTML += textContent
+      });
+    });
+  }
+});
+
+let utterance = new SpeechSynthesisUtterance("Hello world!");
+speechSynthesis.speak(utterance);
--- webpack.config.js
+++ webpack.config.js
@@ -44,4 +44,17 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    fallback: {
+      "zlib": require.resolve('browserify-zlib'),
+      "assert": require.resolve('assert/'),
+      "buffer": require.resolve('buffer/'),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve('util/'),
+      "http": require.resolve('stream-http'),
+      "https": require.resolve('https-browserify'),
+      "fs": require.resolve('fs-web'),
+      "url": require.resolve('URL'),
+    }
+  }
 };
