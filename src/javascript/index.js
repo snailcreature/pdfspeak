459908d4ebe4dbf4cd103f74d0f3b4d455f025be
snailcreature <samuel.drage@myport.ac.uk>
2023-02-17 16:18:20 +0000

    Added text formatting

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,40 +3,39 @@ import '../css/index.css';
 import Worker from './index.worker.js';
 const worker = new Worker();
 
-import fs from 'fs';
-import { parse } from 'path';
 import { getDocument } from 'pdfjs-dist';
-
-console.log("Hello, World!");
-
-// const voices = speechSynthesis.getVoices();
-
-// const voiceList = document.querySelector('#voice-list');
-// voices.forEach((voice) => {
-//   voiceList.innerHTML += `<li>${voice.name}</li>`
-// });
 
 const pdfTextP = document.querySelector('#pdf-text');
 const pdfUploadIpt = document.querySelector('#pdf-upload');
 const pdfReadBttn = document.querySelector('#pdf-read');
-console.log('make a change');
+
 
 pdfReadBttn.addEventListener('click', () => {
   let file;
+  // If there is a file, get the file
   if (pdfUploadIpt.files.length > 0) {
+    pdfTextP.innerHTML = '';
     file = pdfUploadIpt.files[0];
+    // Load the file
     let loadingTask = getDocument(URL.createObjectURL(file));
     loadingTask.promise.then((pdf) => {
       console.log('loaded');
-      pdf.getPage(1).then((page) => {
-        page.getTextContent().then((text) => {
-          console.log(text);
-          text.items.forEach((item) => {
-            if (item.str) pdfTextP.textContent += item.str + ' ';
-          })
+      // Get the pages of the file
+      for (let i = 1; i <= pdf.numPages; i++) {
+        pdf.getPage(i).then((page) => {
+          page.getTextContent().then((text) => {
+            let para = document.createElement('p');
+            text.items.forEach((line) => {
+              if (line.str) para.textContent += line.str + ' ';
+              else {
+                pdfTextP.appendChild(para);
+                para = document.createElement('p');
+              }
+            })
+          });
         });
-      });
-    })
+      }
+    });
   } else {
     console.log('No file');
   }
