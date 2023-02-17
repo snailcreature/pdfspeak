31a2e73af48aee1e62c8e0ed862f7351841c3e68
snailcreature <samuel.drage@myport.ac.uk>
2023-02-17 17:18:58 +0000

    Checkpoint

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,41 +5,51 @@ const worker = new Worker();
 
 import { getDocument } from 'pdfjs-dist';
 
-const pdfTextP = document.querySelector('#pdf-text');
+const pdfEditTxtBx = document.querySelector('#edit-text');
 const pdfUploadIpt = document.querySelector('#pdf-upload');
 const pdfReadBttn = document.querySelector('#pdf-read');
+const readAloudBttn = document.querySelector('#read-aloud');
 
 
 pdfReadBttn.addEventListener('click', () => {
   let file;
+  pdfEditTxtBx.value = '';
   // If there is a file, get the file
   if (pdfUploadIpt.files.length > 0) {
-    pdfTextP.innerHTML = '';
     file = pdfUploadIpt.files[0];
     // Load the file
     let loadingTask = getDocument(URL.createObjectURL(file));
     loadingTask.promise.then((pdf) => {
-      console.log('loaded');
       // Get the pages of the file
-      for (let i = 1; i <= pdf.numPages; i++) {
+      let pageTrack = [...Array(pdf.numPages).keys()].map((value) => {return value + 1});
+      console.log({pageTrack});
+      pageTrack.forEach((i) => {
         pdf.getPage(i).then((page) => {
           page.getTextContent().then((text) => {
-            let para = document.createElement('p');
             text.items.forEach((line) => {
-              if (line.str) para.textContent += line.str + ' ';
-              else {
-                pdfTextP.appendChild(para);
-                para = document.createElement('p');
+              if (line.str) {
+                pdfEditTxtBx.value += line.str + ' ';
               }
-            })
+              else {
+                pdfEditTxtBx.value += '\n\n';
+              }
+            });
+            pdfEditTxtBx.value += '\n\n';
           });
+          page.cleanup();
         });
-      }
+      });
+      console.log('loaded');
     });
   } else {
     console.log('No file');
   }
 });
 
-let utterance = new SpeechSynthesisUtterance("Hello world!");
+readAloudBttn.addEventListener('click', () => {
+  let utterance = new SpeechSynthesisUtterance(pdfEditTxtBx.value);
+  speechSynthesis.speak(utterance);
+});
+
+let utterance = new SpeechSynthesisUtterance("Upload a PDF to begin.");
 speechSynthesis.speak(utterance);
